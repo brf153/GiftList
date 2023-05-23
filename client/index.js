b1205486f24d41e7597f1591a6703bb7ac75446e
brf153 <153hsb@gmail.com>
2023-05-23 14:01:23 +0530

    done week-2 project

--- client/index.js
+++ client/index.js
@@ -6,9 +6,19 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+   const name = 'Norman Block'
+   console.log('name', name)
+   // finding index in the nice list
+   const index = niceList.findIndex(e => e === name );
+   console.log('index', index)
+
+   const merkleTree = new MerkleTree(niceList)
+   const proof = merkleTree.getProof(index)
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: proof,
+    name: name
   });
 
   console.log({ gift });
